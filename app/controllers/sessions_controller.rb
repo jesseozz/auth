57d1429031e6a546eb4058a57930abf1355a8c28
jesse ozdamar <jesse.ozdamar@kellogg.northwestern.edu>
2024-02-20 17:31:36 +0000

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,9 @@ class SessionsController < ApplicationController
     # 2. if the user exists -> check if they know their password
   if @user != nil
     # 3. if they know their password -> login is successful
-    if @user["password"]== params["password"]
+    if BCrypt::Password.new(@user["password"]) == params["password"]
+      cookies["monster"] = "me like cookies"
+      session["user_id"] = @user["id"]
       flash["notice"] = "Welcome."
       redirect_to "/companies"    
     else
@@ -26,6 +28,8 @@ end
 
   def destroy
     # logout the user
+    cookies["monster"] = nil
+    session["user_id"] = nil
     flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,9 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "Kellogg2024"
+encrypted_secret = BCrypt::Password.create(secret)
+puts encrypted_secret
 # 2. prepare encrypted string for testing
+puts BCrypt::Password.new(encrypted_secret) == secret
 # 3. test secret against prepared encrypted string
